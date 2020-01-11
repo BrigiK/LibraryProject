a3229f27ee77b94f0e5e1f5eda1461ffbb5d91ea
BrigiK <kocs_brigitta@yahoo.com>
2020-01-11 21:59:41 +0200

    Removed old projects.

--- DomainModelTests/UnitTest1.cs
+++ DomainModelTests/UnitTest1.cs
@@ -1,14 +1,44 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace DomainModelTests
+﻿//-----------------------------------------------------------------------
+// <copyright file="LibraryTests.cs" company="Transilvania University of Brasov">
+//     Author Kocs Brigitta.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace LibraryTest
 {
+    using System;
+    using System.Linq;
+    using LibraryProject.DataMapper;
+    using LibraryProject.DomainModel;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>Test class for LibraryProject.</summary>
     [TestClass]
-    public class UnitTest1
+    public class LibraryTests
     {
+        /// <summary>Tests if author inserted in database.</summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestIfAuthorInsertedInDb()
         {
+            Author author, testedAuthor;
+            using (var context = new LibraryContext())
+            {
+                author = new Author
+                {
+                    Name = "Jimmy Hen"
+                };
+
+                context.Authors.Add(author);
+                context.SaveChanges();
+            }
+
+            using (var context = new LibraryContext())
+            {
+                testedAuthor = (from auth in context.Authors
+                                where auth.Name == author.Name
+                                select auth).Single();
+            }
+
+            Assert.AreEqual(testedAuthor.Name, author.Name);
         }
     }
 }
